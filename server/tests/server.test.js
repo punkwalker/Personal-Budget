f291f5054adfbbb154e746a19524c41f57526b05
apadyal <apadyal@charlotte.edu>
2023-12-12 14:57:52 -0500

    Added more unit tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,24 +2,49 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../server");
 
-//require("dotenv").config();
-
 const mongoDBuri = "mongodb+srv://apadyal:password%401234@nbad.q1uxsmy.mongodb.net/?retryWrites=true&w=majority";
-
-// /* Connecting to the database before each test. */
-// beforeEach(async () => {
-//     await mongoose.connect(mongoDBuri);
-//   });
-  
-//   /* Closing database connection after each test. */
-//   afterEach(async () => {
-//     await mongoose.connection.close();
-//   });
 
   describe("GET /categories", () => {
     it("should return all budget categories", async () => {
       const res = await request(app).get("/categories");
       expect(res.statusCode).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("GET /user", () => {
+    it("should return a user if user has already signed up", async () => {
+      const res = await request(app).get("/user?userName=a@b.com");
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("GET /budget", () => {
+    it("should return budget for the existing user", async () => {
+      const res = await request(app).get("/budget?userObjectID=6576472fe0e781b0e91a0d4a");
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("GET /expense", () => {
+    it("should return expense for the existing user", async () => {
+      const res = await request(app).get("/expense?userObjectID=6576472fe0e781b0e91a0d4a&month=Jan");
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("POST /expense", () => {
+    it("should save an expense for the existing user", async () => {
+        var expenseData = {
+            userObjectId: "6576472fe0e781b0e91a0d4a",
+            month: "Mar",
+            category: "Travel",
+            expense: 336
+          }
+      const res = await request(app).post("/expense").send(expenseData);
+      expect(res.statusCode).toBe(200);
     });
   });
